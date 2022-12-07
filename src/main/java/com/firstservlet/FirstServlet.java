8575184d4cf73fd0637d2d1c9f8f84008fab3843
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-12-06 21:37:21 -0800

    Added: Create My first Servlet Page

---
+++ src/main/java/com/firstservlet/FirstServlet.java
@@ -0,0 +1,21 @@
+package com.firstservlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3>My First Servlet Page!<h3>");
+        out.close();
+    }
+}
